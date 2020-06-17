438ceef87144f45514c770222ca6d9ccd5d8b4a3
wangxiaohui1988 <18515388187@163.com>
2020-06-17 14:16:55 +0800

    新建项目，配置路由

---
+++ src/views/login/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="login">
+      <h1>登录页面</h1>
+      <el-row>
+        <el-button>默认按钮</el-button>
+        <el-button type="primary">主要按钮</el-button>
+        <el-button type="success">成功按钮</el-button>
+        <el-button type="info">信息按钮</el-button>
+        <el-button type="warning">警告按钮</el-button>
+        <el-button type="danger">危险按钮</el-button>
+      </el-row>
+      <el-row>
+  <el-button plain>朴素按钮</el-button>
+  <el-button type="primary" plain>主要按钮</el-button>
+  <el-button type="success" plain>成功按钮</el-button>
+  <el-button type="info" plain>信息按钮</el-button>
+  <el-button type="warning" plain>警告按钮</el-button>
+  <el-button type="danger" plain>危险按钮</el-button>
+</el-row>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: 'login'
+}
+</script>
+<style lang="scss" scope>
+    #login {
+        height: 100vh;
+        background-color: aqua;
+    }
+</style>
