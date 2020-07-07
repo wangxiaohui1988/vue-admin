dcd50473a786eeba165101bba19725c6b3427ebe
wangxiaohui1988 <18515388187@163.com>
2020-07-07 17:09:55 +0800

    信息分类UI设计

--- src/views/Info/category.vue
+++ src/views/Info/category.vue
@@ -1,9 +1,215 @@
 <template>
-  <div>信息分类</div>
+  <div id="category">
+    <el-button type="danger">添加一级分类</el-button>
+    <hr class="hr-e9e9e9" />
+    <div>
+      <el-row :gutter="30">
+        <el-col :span="8">
+          <div class="category-wrap">
+            <div class="category">
+              <!--一级分类-->
+              <h4>
+                <svg-icon icon-class="plus"></svg-icon>
+                新闻
+                <el-button type="danger" size="mini" round>编辑</el-button>
+                <el-button type="success" size="mini" round>添加子级</el-button>
+                <el-button size="mini" round>删除</el-button>
+              </h4>
+              <!-- 子级分类 -->
+              <ul>
+                <li>
+                  国内
+                  <div class="button-group">
+                    <el-button size="mini" type="danger" round>编辑</el-button>
+                    <el-button size="mini" round>删除</el-button>
+                  </div>
+                </li>
+                <li>
+                  国内
+                  <div class="button-group">
+                    <el-button size="mini" type="danger" round>编辑</el-button>
+                    <el-button size="mini" round>删除</el-button>
+                  </div>
+                </li>
+                <li>
+                  国内
+                  <div class="button-group">
+                    <el-button size="mini" type="danger" round>编辑</el-button>
+                    <el-button size="mini" round>删除</el-button>
+                  </div>
+                </li>
+                <li>
+                  国内
+                  <div class="button-group">
+                    <el-button size="mini" type="danger" round>编辑</el-button>
+                    <el-button size="mini" round>删除</el-button>
+                  </div>
+                </li>
+                <li>
+                  国内
+                  <div class="button-group">
+                    <el-button size="mini" type="danger" round>编辑</el-button>
+                    <el-button size="mini" round>删除</el-button>
+                  </div>
+                </li>
+              </ul>
+              <!--一级分类-->
+              <h4>
+                <svg-icon icon-class="plus"></svg-icon>
+                新闻
+              </h4>
+              <!-- 子级分类 -->
+              <ul>
+                <li>
+                  国内
+                  <div class="button-group">
+                    <el-button size="mini" type="danger" round>编辑</el-button>
+                    <el-button size="mini" round>删除</el-button>
+                  </div>
+                </li>
+                <li>
+                  国内
+                  <div class="button-group">
+                    <el-button size="mini" type="danger" round>编辑</el-button>
+                    <el-button size="mini" round>删除</el-button>
+                  </div>
+                </li>
+                <li>
+                  国内
+                  <div class="button-group">
+                    <el-button size="mini" type="danger" round>编辑</el-button>
+                    <el-button size="mini" round>删除</el-button>
+                  </div>
+                </li>
+                <li>
+                  国内
+                  <div class="button-group">
+                    <el-button size="mini" type="danger" round>编辑</el-button>
+                    <el-button size="mini" round>删除</el-button>
+                  </div>
+                </li>
+                <li>
+                  国内
+                  <div class="button-group">
+                    <el-button size="mini" type="danger" round>编辑</el-button>
+                    <el-button size="mini" round>删除</el-button>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </el-col>
+        <el-col :span="16">
+          <h4 class="menu-title">一级分类编辑</h4>
+          <el-form :model="ruleForm"  ref="ruleForm" label-width="142px" class="from-wrap">
+            <el-form-item label="一级分类名称" prop="categoryName">
+              <el-input v-model="ruleForm.categoryName" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="二级分类名称" prop="secCategoryName">
+              <el-input v-model="ruleForm.secCategoryName" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="danger">确定</el-button>
+            </el-form-item>
+          </el-form>
+        </el-col>
+      </el-row>
+    </div>
+  </div>
 </template>
 <script>
+import { reactive } from '@vue/composition-api'
 export default {
+  name: 'category',
+  setup (props, { root }) {
+    const ruleForm = reactive({
+      categoryName: '',
+      secCategoryName: '',
+      age: ''
+    })
+
+    return { ruleForm }
+  }
 }
 </script>
 <style lang="scss" scope>
+@import "../../styles/config.scss";
+.category-wrap {
+    div:first-child {
+        &:before { top: 20px; }
+    }
+    div:last-child {
+        &:before { bottom: 21px; }
+    }
+}
+.menu-title {
+    line-height: 44px;
+    padding-left: 22px;
+    background-color: #f3f3f3;
+}
+.category {
+    position: relative;
+    line-height: 44px;
+    cursor: pointer;
+    &:before {
+        content: '';
+        position: absolute;
+        left: 22px;
+        top: 0;
+        bottom: 0;
+        width: 32px;
+        border-left: 1px dotted #000;
+    }
+    h4 {
+        position: relative;
+        padding-left: 40px;
+    }
+    svg {
+        position: absolute;
+        left: 14px;
+        top: 15px;
+        background-color: #fff;
+        font-size: 17px;
+    }
+    li {
+        position: relative;
+        padding-left: 36px;
+        margin-left: 24px;
+        &:before {
+            content: '';
+            position: absolute;
+            left: 0;
+            top: 22px;
+            width: 32px;
+            border-bottom: 1px dotted #000;
+        }
+    }
+    li, h4 {
+        @include webkit(transition, all .5s ease 0s);
+        &:hover {
+            background-color: #f3f3f3;
+            .button-group { display: block; }
+        }
+    }
+}
+.button-group {
+    display: none;
+    position: absolute;
+    z-index: 2;
+    right: 11px;
+    top: -1px;
+    button { font-size: 12px; }
+}
+.from-wrap {
+    width: 410px;
+    padding-top: 26px;
+}
+.hr-e9e9e9 {
+    width: calc(100% + 60px);
+    margin-left: -30px;
+    border: none;
+    margin-top: 30px;
+    margin-bottom: 30px;
+    border-bottom: 1px solid #e9e9e9;
+}
 </style>
